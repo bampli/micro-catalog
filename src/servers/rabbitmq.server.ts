b62a8d0df466593ff3034e63d1ebec20e525eaf0
Jose Motta <josemotta@bampli.com>
2022-02-28 13:14:41 -0300

    conectando aplicacao com exchanges & queues

--- src/servers/rabbitmq.server.ts
+++ src/servers/rabbitmq.server.ts
@@ -1,6 +1,6 @@
 import {Context} from "@loopback/context";
 import {Server} from "@loopback/core";
-import {connect, Connection} from "amqplib";
+import {Channel, connect, Connection} from "amqplib";
 
 export class RabbitmqServer extends Context implements Server {
   private _listening: boolean;
@@ -13,7 +13,22 @@ export class RabbitmqServer extends Context implements Server {
       password: 'admin'
     });
     this._listening = true;
-    return undefined;
+    this.boot();
+  }
+
+  async boot() {
+    const channel: Channel = await this.conn.createChannel();
+    const queue = await channel.assertQueue('first-queue');
+    const exchange = await channel.assertExchange('amq-direct', 'direct');
+
+    await channel.bindQueue(queue.queue, exchange.exchange, 'my-routing-key');
+
+    //const result = channel.sendToQueue('first-queue', Buffer.from('hello world !'));
+
+    channel.publish('amq-direct', 'my-routing-key', Buffer.from('publicado via routing key'));
+
+    channel.consume(queue.queue, (message) => console.log(message?.content.toString()));
+    //console.log(result);
   }
 
   async stop(): Promise<void> {
