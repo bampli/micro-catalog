a0fef5d89fefd4b660567e6cfe2595c2c4b967cc
Jose Motta <josemotta@bampli.com>
2022-02-28 18:46:02 -0300

    criando uma categoria ES quando mensagem recebida

--- src/servers/rabbitmq.server.ts
+++ src/servers/rabbitmq.server.ts
@@ -1,6 +1,9 @@
 import {Context} from "@loopback/context";
 import {Server} from "@loopback/core";
-import {Channel, connect, Connection} from "amqplib";
+import {repository} from "@loopback/repository";
+import {Channel, connect, Connection, Replies} from "amqplib";
+import {Category} from '../models';
+import {CategoryRepository} from "../repositories";
 
 // Comunicação usando rabbitmq:
 // - disparar uma mensagem a cada evento de cada model do Laravel:
@@ -13,6 +16,11 @@ export class RabbitmqServer extends Context implements Server {
   private _listening: boolean;
   conn: Connection;
 
+  constructor(@repository(CategoryRepository) private categoryRepo: CategoryRepository) {
+    super();
+    //console.log(this.categoryRepo);
+  }
+
   async start(): Promise<void> {
     this.conn = await connect({
       hostname: 'rabbitmq',
@@ -20,26 +28,44 @@ export class RabbitmqServer extends Context implements Server {
       password: 'admin'
     });
     this._listening = true;
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
     this.boot();
   }
 
   async boot() {
     const channel: Channel = await this.conn.createChannel();
-    const queue = await channel.assertQueue('micro-catalog/sync-videos');
-    const exchange = await channel.assertExchange('amq.topic', 'topic');
+    const queue: Replies.AssertQueue = await channel.assertQueue('micro-catalog/sync-videos');
+    const exchange: Replies.AssertExchange = await channel.assertExchange('amq.topic', 'topic');
 
     await channel.bindQueue(queue.queue, exchange.exchange, 'model.*.*');
 
-    //const result = channel.sendToQueue('first-queue', Buffer.from('hello world !'));
-    //channel.publish('amq-direct', 'my-routing-key', Buffer.from('publicado via routing key'));
-
     channel.consume(queue.queue, (message) => {
       if (!message) {return };
-      //console.log(JSON.parse(message?.content.toString()))
+      //console.log(message);
+      const data: Category = JSON.parse(message.content.toString());
       const [model, event] = message.fields.routingKey.split('.').slice(1);
-      console.log(model, event);
-    });
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      this.sync({model, event, data});
+      //console.log(model, event);
+    }).then(() => { }).catch(() => { });
     //console.log(result);
+  }
+
+  async sync({model, event, data}: {model: string, event: string, data: Category}) {
+    if (model === 'category') {
+      switch (event) {
+        case 'created':
+          await this.categoryRepo.create({
+            ...data,
+            created_at: new Date(),
+            updated_at: new Date()
+          });
+          break;
+        case 'deleted':
+          console.log("delete a category");
+          break;
+      }
+    }
   }
 
   async stop(): Promise<void> {
