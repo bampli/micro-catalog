0ba0b52b962664606fb9efc4320869173461bb1f
Jose Motta <josemotta@bampli.com>
2022-03-14 12:42:38 -0300

    criando regras validacao personalizadas 1

--- src/application.ts
+++ src/application.ts
@@ -5,7 +5,7 @@ import {RestComponent, RestServer} from '@loopback/rest';
 import {RestExplorerBindings} from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
-import {RestExplorerComponent} from './components';
+import {RestExplorerComponent, ValidatorsComponent} from './components';
 import {Category} from './models';
 import {MySequence} from './sequence';
 import {RabbitmqServer} from './servers';
@@ -33,7 +33,7 @@ export class MicroCatalogApplication extends BootMixin(
       path: '/explorer',
     });
     this.component(RestExplorerComponent);
-
+    this.component(ValidatorsComponent);
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
     this.bootOptions = {
