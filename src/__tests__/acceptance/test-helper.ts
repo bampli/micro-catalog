57c30f0fda294a56d74e26d9eb3e2bdb96f41149
Jose Motta <josemotta@bampli.com>
2022-04-04 17:15:57 -0300

    criando teste CategoryController

--- src/__tests__/acceptance/test-helper.ts
+++ src/__tests__/acceptance/test-helper.ts
@@ -3,6 +3,8 @@ import {MicroCatalogApplication} from '../..';
 import config from '../../../config';
 //import { supertest } from '@loopback/testlab';
 import supertest from 'supertest';
+import {Esv7DataSource} from '../../datasources';
+import dbConfig from '../../datasources/esv7.datasource.config';
 
 export async function setupApplication(): Promise<AppWithClient> {
   const restConfig = givenHttpServerConfig({
@@ -20,11 +22,23 @@ export async function setupApplication(): Promise<AppWithClient> {
   });
 
   await app.boot();
+
+  app.bind('datasources.esv7').to(testDb);
+
   await app.start();
 
   const client = supertest('http://127.0.0.1:9000');
 
   return {app, client};
+}
+
+export const testDb = new Esv7DataSource({
+  ...dbConfig,
+  index: 'catalog-test',
+});
+
+export async function clearDb() {
+  await testDb.deleteAllDocuments();
 }
 
 export interface AppWithClient {
